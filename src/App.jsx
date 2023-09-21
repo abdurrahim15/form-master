07dff30d0ba1de6c440f3ca9e7eb5390c2e16cb3
abdurrahim15 <abdurrahimh3@gmail.com>
2023-09-21 16:08:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+// import SimpleForm from './Components/SimpleForm/simpleForm'
+import StatefulForm from './Components/StatefulForm/StatefulForm'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      <StatefulForm></StatefulForm>
+      
+    </>
+  )
+}
+
+export default App
